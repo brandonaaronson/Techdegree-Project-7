891063d9ee85678744d4d353a3558a81eace5573
Brandon Aaronson <brandonaaronson@Brandons-MacBook-Pro-2.local>
2019-05-23 13:43:27 -0700

    Updates

--- dashboard.js
+++ dashboard.js
@@ -1,20 +1,19 @@
 
-const alert = document.getElementById('alert');
+const alertNotification = document.getElementById('alert');
 
 // -- creates the html for the banner --//
 
-alert.innerHTML =
+alertNotification.innerHTML =
 `
 <div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
-to complete</p>
+<p><strong>Alert:</strong> Lorem ipsum dolor sit amet, perfecto honestatis at usu, te.</p>
 <p class="alert-banner-close">X</p>
 </div>
 `
-alert.addEventListener('click', e => {
+alertNotification.addEventListener('click', e => {
   const element = e.target;
   if (element.classList.contains('alert-banner-close')) {
-    alert.style.display = 'none';
+    alertNotification.style.display = 'none';
     return false;
   }
 });
@@ -25,13 +24,13 @@ const user = document.getElementById('userField');
 const message = document.getElementById('messageField');
 const send = document.getElementById('send');
 
-send.addEventListener('click', () => {
-  
+send.addEventListener('click', e => {
+  e.preventDefault();
   if (user.value === "" && message.value === "") {
     alert("Please fill out user and message fields before sending");
   } else if (user.value === "") {
     alert('Please fill out user field before sending');
-  } else if (message === "") {
+  } else if (message.value === "") {
     alert("Please fill out message field before sending");
   } else {
     alert(`Message successfully sent to: ${user.value}`);
